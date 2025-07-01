327bf80fc3f1d028aed8b0d91735b2d0065627df
Gorachand Senapati <gorachandsenapati8@gmail.com>
2025-07-01 17:11:19 +0530

    second process

--- Trees/BST/Delete Node in BST.cpp
+++ Trees/BST/Delete Node in BST.cpp
@@ -50,3 +50,42 @@ public:
 };
 
 //I also possible for Inorder predecessor which is right most node in left subtree
+class Solution {
+public:
+
+   TreeNode* getInorderSuccessor(TreeNode* root){//right most node in left subtree
+    while(root != NULL && root->right!= NULL){
+        root = root->right;
+    }
+    return root;
+   }
+    TreeNode* deleteNode(TreeNode* root, int key) {//key is the value which to delete
+    if(root == NULL){
+        return NULL;
+    }
+        if(key >root->val){
+          root->right = deleteNode(root->right, key);
+        } else if(key < root->val){
+           root->left = deleteNode(root->left, key);
+        } else{
+            //delete here root == key
+            //1) If parent have 0 or 1 child so combine both
+            if(root->left == NULL){
+                TreeNode* temp = root-> right;
+                delete root;
+                return temp;
+            } else if(root->right == NULL){
+                TreeNode* temp = root-> left;
+                delete root;
+                return temp;
+            } else {//2 children 
+               TreeNode* IS = getInorderSuccessor(root->left);
+               root->val = IS->val;
+               root->left = deleteNode(root->left, IS->val);
+
+            }
+        }
+
+        return root;
+    }
+};
