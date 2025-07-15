ac1081b26da9abc14c131794968855e034575912
Gorachand Senapati <gorachandsenapati8@gmail.com>
2025-07-15 23:07:45 +0530

    string problem

---
+++ Strings/reverse a string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int st=0, end=n-1;
+        while(st<end) {
+            swap(s[st], s[end]);
+            st++;
+            end--;
+
+            //  for single line
+
+            // swap(s[st++], s[end--]);
+        } //char array 
+        
+        // reverse(s.begin(), s.end()); //string
+        
+    }
+};
