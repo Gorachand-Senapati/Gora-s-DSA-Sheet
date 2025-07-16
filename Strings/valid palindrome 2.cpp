a2c4ae65dbce65169be2dccd8b2dd7d27c73fac4
Gorachand Senapati <gorachandsenapati8@gmail.com>
2025-07-16 08:59:56 +0530

    problem

--- Strings/valid palindrome 2.cpp
+++ Strings/valid palindrome 2.cpp
@@ -13,7 +13,7 @@ public:
         int left = 0, right = s.size() - 1;
         while (left < right) {
             if (s[left] != s[right]) {
-                // Try removing one character either from left or right
+                // Try removing one character either from left or right here skip
                 return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
             }
             left++;
