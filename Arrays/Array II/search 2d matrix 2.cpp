560d6a5875685b1ab59392d6de41a75c77d66db7
Gorachand Senapati <gorachandsenapati8@gmail.com>
2025-07-19 00:36:58 +0530

    add

--- Arrays/Array II/search 2d matrix 2.cpp
+++ Arrays/Array II/search 2d matrix 2.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
-        int lowerst = matrix[0][0], highest = matrix[m-1][n-1];
+        int lowest = matrix[0][0], highest = matrix[m-1][n-1];
         int row = 0 , col = n-1;
 
       while(row<m && col>=0) { 
