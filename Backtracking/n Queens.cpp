14fad1ebd1d31904d9665226aab414dab0689c55
Gorachand Senapati <gorachandsenapati8@gmail.com>
2025-07-22 12:45:35 +0530

    n queens

---
+++ Backtracking/n Queens.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+ bool isSafe(vector<string>& board, int row, int col, int n) { //O(n)
+    //horizontal 
+    for(int j=0; j<n; j++){
+        if(board[row][j] == 'Q'){
+            return false;
+        }
+    }
+    //vertical
+     for(int i=0; i<n; i++){
+        if(board[i][col] == 'Q'){
+            return false;
+        }
+    }
+    //left diagonal
+    for(int i=row , j= col; i>=0 && j>= 0; i--, j--) {
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+
+    //right diagonal
+    for(int i=row , j= col; i>=0 && j<=n; i--, j++) {
+        if(board[i][j] == 'Q'){
+            return false;
+        }
+    }
+    return true;
+
+ }
+
+ void nQueens(vector<string>& board, int row, int n,vector<vector<string>> &ans ) {
+    if(row == n) { //base case
+        ans.push_back({board});
+        return;
+    }
+
+    for(int j=0; j<n;j++) {
+        if(isSafe(board, row, j,n)){
+            board[row] [j] = 'Q';
+            nQueens(board, row+1, n , ans);//next row next queen
+            board[row] [j] = '.';//backtracking and convert position .(empty)
+        }
+    }
+ }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n,string(n, '.')); //start a string all position is now empty which size equal queen
+        vector<vector<string>> ans;
+        nQueens(board,0, n, ans);
+        return ans;
+    }
+};
