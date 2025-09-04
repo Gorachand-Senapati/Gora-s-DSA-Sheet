9d0e71aa8a3fa970c8fc34d937d4213d7f8c1fbd
Gorachand Senapati <gorachandsenapati8@gmail.com>
2025-09-04 09:57:20 +0530

    optimal

--- sliding window and 2 pointer/longest repeating charcter replacement.cpp
+++ sliding window and 2 pointer/longest repeating charcter replacement.cpp
@@ -18,3 +18,24 @@ public:
         return maxLen;
     }
 };
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0, r = 0, maxLen = 0, maxFreq = 0;
+        int freq[26] = {0};
+
+        while (r < s.size()) {//O(n)
+            freq[s[r] - 'A']++;
+            maxFreq = max(maxFreq, freq[s[r] - 'A']); // track max freq seen
+            // if changes needed > k, shrink window
+            if ((r - l + 1) - maxFreq > k) { //here while using the TC= O(2n) becouse while loop never go always n times but small part go to n so
+                freq[s[l] - 'A']--;
+                l++;
+            }
+            maxLen = max(maxLen, r - l + 1);
+            r++;
+        }
+        return maxLen;
+    }
+};
