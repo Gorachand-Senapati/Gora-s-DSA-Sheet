aa1d9ef53559ce9ba4654462c1a0517d33d22f12
Gorachand Senapati <gorachandsenapati8@gmail.com>
2025-12-24 17:39:16 +0530

    ok

--- Graphs/Shortest path algorithm and problems/Find the City With the Smallest Number of Neighbors at a Threshold Distance.cpp
+++ Graphs/Shortest path algorithm and problems/Find the City With the Smallest Number of Neighbors at a Threshold Distance.cpp
@@ -38,3 +38,64 @@ public:
 }; //using floyed warshall TC= O(n^3) SC= O(n^2)
 
 //dijkstra approach
+// User function Template for C++
+class Solution {
+  public:
+   void dijkstra(int n,vector<vector<pair<int,int>>>&adj ,
+   vector<int>&dist, int src ){
+       priority_queue<pair<int,int>, vector<pair<int,int>>,
+       greater<pair<int,int>>>pq;//for dijkstra need pq
+       
+       pq.push({0,src});//src to src dist 0
+       fill(dist.begin(), dist.end(), 1e9);//fill the dist array with 1e9
+       dist[src]=0;
+       
+       while(!pq.empty()){
+           int dis = pq.top().first;
+           int node = pq.top().second;
+           pq.pop();
+           for(auto it: adj[node]){
+               int v = it.first;
+               int wt = it.second;
+               if(dis +wt <  dist[v]){
+                   dist[v]= dis+wt;
+                   pq.push({dist[v], v});
+               }
+           }
+       }
+   }
+    int findCity(int n, int m, vector<vector<int>>& edges, int distanceThreshold) {
+        // Your code here
+        vector<vector<int>>matrix(n,vector<int>(n,1e9));//create a matrix
+        for(int i=0;i<n;i++) matrix[i][i]=0;
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto it: edges){
+            int u = it[0];
+            int v= it[1];
+            int wt = it[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});//for undirected
+        }
+        //check for all node are source apply dijkstra
+        for(int i=0;i<n;i++){
+            dijkstra(n, adj,matrix[i], i);//vertex, edges, adjlist, row,src
+        }
+        //find the city by iterating over matrix
+         int cntCity = n;
+        int cityNo= -1;//no city visited
+        for(int city=0;city<n;city++){
+            int cnt =0;
+            for(int adCit =0; adCit<n;adCit++){
+                if(matrix[city][adCit] <= distanceThreshold){
+                    cnt++;
+                }
+            }
+            if(cnt<= cntCity){
+                cntCity= cnt;
+                cityNo= city;
+            }
+        }
+        return cityNo;
+    }
+};
+//TC= O(n*(E + V)logV) SC= O(n^2)
