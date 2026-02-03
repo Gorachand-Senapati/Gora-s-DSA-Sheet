ea5cc0186d0ed29ad3ca43a3963a5a99f1c30408
Gorachand Senapati <gorachandsenapati8@gmail.com>
2026-02-04 00:01:59 +0530

    ok

---
+++ Daily most improtant ques/Divide an array into subarrays with minimum cost I.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int n= nums.size();
+        int min1 =100, min2 =100;//here max stay 50 so assume the large vlaue than 50
+        for(int i=1;i<n;i++){
+            if(nums[i] < min1){
+                min2 =min1;
+                min1 = nums[i];
+            }else if(nums[i] < min2){
+                min2 = nums[i];
+            }
+        }
+        return nums[0] + min1 + min2;
+    }
+};
