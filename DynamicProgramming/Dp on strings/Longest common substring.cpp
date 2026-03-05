d4ed4b630d15eda00ade5eb99fb989d9e17c92f1
Gorachand Senapati <gorachandsenapati8@gmail.com>
2026-03-05 21:00:22 +0530

    dp

--- DynamicProgramming/Dp on strings/Longest common substring.cpp
+++ DynamicProgramming/Dp on strings/Longest common substring.cpp
@@ -1,3 +1,30 @@
+//memoization
+class Solution {
+  public:
+     int ans=0;
+     int f(int i,int j,string& s1, string& s2, vector<vector<int>>&dp){
+         if(i <0 || j <0) return 0;
+         int len = 0;
+         if(dp[i][j] != -1) return dp[i][j];
+         if(s1[i] == s2[j]){
+             len = 1 + f(i-1,j-1,s1,s2,dp);
+             ans = max(len,ans);
+         } 
+         //if not match
+         f(i-1,j,s1,s2,dp);
+         f(i,j-1,s1,s2,dp);
+         return dp[i][j]= len;
+     }
+    int longCommSubstr(string& s1, string& s2) {
+        // your code here
+        int n = s1.size(),m = s2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+         f(n-1,m-1,s1,s2,dp);
+         return ans;
+    }
+};
+
+
 //tabulation 
 class Solution {
   public:
